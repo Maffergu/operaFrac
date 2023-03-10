3aa852903ed3552ff770366bdfee6a4502dda594
Maffergu <60632469+Maffergu@users.noreply.github.com>
2023-03-10 09:55:12 -0600

    cambios

--- fracciones/calculadora/urls.py
+++ fracciones/calculadora/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
     path('suma', views.suma, name='suma'),
     path('resta', views.resta, name='resta'),
     path('division', views.division, name='division'),
+    path('usuarios', views.usuarios, name = 'usuarios'),
+    path('usuarios_p', views.usuarios_p, name = 'usuarios_p'),
+    path('usuarios_d', views.usuarios_d, name = 'usuarios_d'),
 ]
--- fracciones/calculadora/views.py
+++ fracciones/calculadora/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt 
 from json import loads, dumps
+import sqlite3
 
 class Fraccion:
     def __init__(self,num,den):
@@ -81,6 +82,37 @@ def division(request):
     return HttpResponse(json_res,content_type="text/json-comment-filtered")
     #return HttpResponse("La division de " +numerador1+ "/"+denominador1+"/" +numerador2+ "/"+denominador2+"= " + str(r1)+"/"+str(r2))
 
+def usuarios(request):
+    con = sqlite3.connect("db.sqlite3")
+    cur = con.cursor()
+    res =cur.execute("SELECT * FROM usuarios")
+    resultado = res.fetchall()
+  
+    return render(request, 'datos.html', {'resultado':resultado})
 #Entrada: {"numerador1":1,"denominador1":2,"numerador2":1,"denominador2":2}
   
 #http://127.0.0.1:8000/division?numerador1=1&denominador1=4&numerador2=1&denominador2=5
+
+@csrf_exempt
+def usuarios_p(request):
+    body = request.body.decode('UTF-8')
+    eljson = loads(body)
+    grado = eljson['grado']
+    grupo = eljson['grupo']
+    num_lista = eljson['num_lista']
+    con = sqlite3.connect("db.sqlite3")
+    cur = con.cursor()
+    res = cur.execute("INSERT INTO usuarios (grupo, grado, num_lista) VALUES (?,?,?)",(grupo, grado, num_lista))
+    con.commit()
+    return HttpResponse(grado+grupo+num_lista)
+
+@csrf_exempt
+def usuarios_d(request):
+   body = request.body.decode('UTF-8')
+   eljson = loads(body)
+   id = eljson['id']
+   con = sqlite3.connect("db.sqlite3")
+   cur = con.cursor()
+   res = cur.execute("DELETE FROM usuarios WHERE id_usuario=?",(str(id)))
+   con.commit()
+   return HttpResponse('OK usuario borrado'+str(id))
